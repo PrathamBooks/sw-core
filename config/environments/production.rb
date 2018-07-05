b96d924f23a90487ba7b72fa710fbae4f339a31e
Alok Kuchlous <alokk@mirafra.com>
2018-07-05 10:39:10 +0530

    Remove old credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
     :address   => "smtp.mandrillapp.com",
     :port      => 2525, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "info@prathambooks.org",
-    :password  => "mrGhyMqrtB5IijRSxDRyAg", # SMTP password is any valid API key
+    :user_name => "",
+    :password  => "", # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
     :domain => 'prathambooks.org', # your domain to identify your server when connecting
     :raise_delivery_errors => true
